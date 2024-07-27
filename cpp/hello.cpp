d97daa2255469c0dd1c748018fbfc6691baf28ef
Sreehari Sujith <sreehari9300@gmail.com>
2024-07-27 23:39:06 +0530

    initial commit

---
+++ cpp/hello.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+cout<<"hey"<<endl;
+
+return 0;
+}
---
+++ hash.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class hash{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n=scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            System.out.println("enter the contents into the array\n");
+            arr[i]=scanner.nextInt();
+
+        }
+        int[]hash=new int[100];
+        for(int i=0;i<n;i++){
+            hash[arr[i]]+=1;
+        }
+
+        System.out.println("enter the number of querries\n");
+        int q=scanner.nextInt();
+
+        System.out.println("enter the querries\n");
+        for(int i=0;i<q;i++){
+            int number = scanner.nextInt();
+            if(number<hash.length){
+                System.out.printf("the numeber %d appears in the array %d times \n",number,hash[number]);
+            }else{
+                System.out.println("array out of bound index\n");
+            }
+        }
+
+scanner.close();
+    
+
+    }
+}
